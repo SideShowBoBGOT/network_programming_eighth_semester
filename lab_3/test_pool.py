e410349e4461e7873a7cea67fe0f09d8f9a72c98
account <account@mail.com>
2025-03-18 16:23:17 +0200

    pool and parallel work

---
+++ lab_3/test_pool.py
@@ -0,0 +1,32 @@
+import subprocess
+import pathlib
+import os
+import time
+
+SCRIPT_DIR = pathlib.Path(os.path.dirname(os.path.abspath(__file__)))
+BUILD_DIR = SCRIPT_DIR / 'build'
+CLIENT_EXECUTABLE = BUILD_DIR / 'client.o'
+SERVER_EXECUTABLE = BUILD_DIR / 'pool_server.o'
+ADDRESS  = '0.0.0.0'
+PORT = '55002'
+MAX_FILE_SIZE = '1000000000'
+MAX_CLIENTS = '100'
+
+BOOKS_DIR = pathlib.Path('/home/sideshowbobgot/university/C')
+
+server = subprocess.Popen([SERVER_EXECUTABLE, ADDRESS, PORT, BOOKS_DIR, MAX_CLIENTS])
+
+clients: list[subprocess.Popen[bytes]] = []
+for dirpath, dirnames, filenames in os.walk(BOOKS_DIR):
+    for file in filenames:
+        if file.endswith('.pdf'):
+            clients.append(subprocess.Popen([CLIENT_EXECUTABLE, ADDRESS, PORT, file, MAX_FILE_SIZE]))
+
+for client in clients:
+    client.wait()
+
+time.sleep(1)
+
+print('[CLIENTS FINISHED]')
+
+server.wait()
